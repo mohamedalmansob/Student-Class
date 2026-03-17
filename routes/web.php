b274f5c5d90ec159f36e40a82cd592dc569ab9ca
mohamedalmansob <almansopemails@gmail.com>
2026-03-17 22:35:08 +0300

    add Router create and store and edit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CorsesController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
@@ -7,3 +8,8 @@ use Illuminate\Support\Facades\View;
 Route::get('/', [HomeController::class,'index'])->name('home');
 
 
+Route::get('Corses',[CorsesController::class,'index'])->name('Corses.index');
+Route::get('Create_Corses',[CorsesController::class,'create'])->name('Corses.Create');
+Route::post('Store_Corses',[CorsesController::class,'store'])->name('Corses.store');
+Route::get('Edit_Corses',[CorsesController::class,'edit'])->name('Corses.edit');
+Route::get('Corses',[CorsesController::class,'index'])->name('Corses.index');
