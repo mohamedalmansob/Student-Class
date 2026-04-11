220b19c273441dee37047fc113371d7c8e2c34a7
mohamedalmansob <almansopemails@gmail.com>
2026-04-11 16:47:30 +0300

    add data fields student table to model students

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Students extends Model
 {
-    //
+    protected $table ='student';
+    //protected $fillable = ['name','active','created_at','updated_at'];
+    protected $guarded = [];
 }
