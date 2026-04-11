c0453953de03bf76934444e75b83077563b7745f
mohamedalmansob <almansopemails@gmail.com>
2026-04-11 16:58:52 +0300

    add methode index to StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Students;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index(){
+        $data=Students::all();
+        return View('Students.index',['data'=>$data]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,13 @@
 
 use App\Http\Controllers\CorsesController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 
-
+//courses
 Route::get('Corses',[CorsesController::class,'index'])->name('Corses.index');
 Route::get('Create_Corses',[CorsesController::class,'create'])->name('Corses.Create');
 Route::post('Store_Corses',[CorsesController::class,'store'])->name('Corses.store');
@@ -15,3 +16,12 @@ Route::get('Edit_Corses/{id}',[CorsesController::class,'edit'])->name('Corses.ed
 Route::get('Corses',[CorsesController::class,'index'])->name('Corses.index');
 Route::post('‘Update_Corses/{id}',[CorsesController::class,'update'])->name('Corses.update');
 Route::get('Delete_Corses/{id}',[CorsesController::class,'delete'])->name('Corses.delete');
+
+//Students
+Route::get('Students',[StudentController::class,'index'])->name('Students.index');
+Route::get('Create_Students',[StudentController::class,'create'])->name('Students.Create');
+Route::post('Store_Students',[StudentController::class,'store'])->name('Students.store');
+Route::get('Edit_Students/{id}',[StudentController::class,'edit'])->name('Students.edit');
+Route::get('Students',[StudentController::class,'index'])->name('Students.index');
+Route::post('‘Update_Students/{id}',[StudentController::class,'update'])->name('Students.update');
+Route::get('Delete_Students/{id}',[StudentController::class,'delete'])->name('Students.delete');
