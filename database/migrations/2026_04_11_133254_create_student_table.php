cf3d6fe4e0bf4fa9b74d8f453ea0f3bdbfb48bdc
mohamedalmansob <almansopemails@gmail.com>
2026-04-11 16:42:36 +0300

    add fildes to student table

--- database/migrations/2026_04_11_133254_create_student_table.php
+++ database/migrations/2026_04_11_133254_create_student_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
     {
         Schema::create('student', function (Blueprint $table) {
             $table->id();
-            $table->string('name',255);
-            $table->string('phond',100);
-            $table->string('address',255);
-            $table->string('image',255);
+            $table->string('name',225);
+            $table->string('phones',225);
+            $table->string('address',225);
+            $table->string('image',225);
+            $table->string('nutionalID',30);
+            $table->string('notes',225);
             $table->tinyInteger('active')->default(1)->comment('هل الطالب مفعل او معطل');
             $table->foreignId('country_id')->references('id')->on('countries')->onUpdate('cascade');
-
-
             $table->timestamps();
         });
     }
