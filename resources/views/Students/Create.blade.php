92c0cb3d96f18a2fddae889d3b03950f0b6ac556
mohamedalmansob <almansopemails@gmail.com>
2026-04-11 17:01:38 +0300

    add view Students to file Students

---
+++ resources/views/Students/Create.blade.php
@@ -0,0 +1,45 @@
+@extends('master')
+@section('titel')
+    اضافة كورس جديد
+@endsection
+@section('content')
+<div class="col-md-12">
+  {{-- عرض الرسالة --}}
+  @if (Session::has('error'))
+  <div class="alert alert-error" role="alert">
+      {{ Session::get('error') }}
+    </div>
+  @endif
+<form method="POST" action="{{ route('Corses.store') }}" role="form" style="width: 80%;margin: 0 auto;background-color: white">
+  @csrf
+    <div class="card-body">
+      <div class="form-group">
+        <label for="exampleInputEmail1">اسم الكورس</label>
+        <input autofocus type="text" name="name" class="form-control" id="name" placeholder="Enter email" value="{{ old('name') }}">
+        @error('name')
+           <span style="color: red">{{ $message }}</span> 
+        @enderror
+      </div>
+      <div class="form-group">
+        <label for="">حالة التفعيل</label>
+         <select name="active" id="active" name="active" class="form-control">
+          <option value="">اختر الحالة</option>
+          <option value="1" @if (old('active')==1) selected @endif>مفعلة</option>
+          <option value="0"@if (old('active')==0 and old('active')!='') selected @endif>معطلة</option>
+         </select>
+         @error('active')
+         <span style="color: red">{{ $message }}</span> 
+      @enderror
+      </div>
+     
+     
+    
+    <!-- /.card-body -->
+
+    <div style="text-align: center" class="form-group">
+      <button type="submit" class="btn btn-primary">اضافة كورس</button>
+    </div>
+  </div>
+  </form>
+</div>
+@endsection
---
+++ resources/views/Students/index.blade.php
@@ -0,0 +1,65 @@
+@extends('master')
+
+@section('titel')
+    الكورسات
+@endsection
+@section('content')
+    <div class="col-12" style="background-color: white;padding: 15px">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title" style="text-align: center;float: none;">بيانات الكورسات
+         <a href="{{ route('Corses.Create') }}" class="btn btn:sm btn-info">اضافة جديد</a>
+          </h3>
+        @if (Session::has('success'))
+        <div class="alert alert-success" role="alert">
+            {{ Session::get('success') }}
+          </div>
+        @endif
+        @if (Session::has('error'))
+        <div class="alert alert-error" role="alert">
+            {{ Session::get('error') }}
+          </div>
+        @endif
+    
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive p-0" style="height: 300px;">
+            @if (@isset($data) and !@empty($data) and @count($data)>0 )
+                
+           
+            <table id="example2" class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>اسم الكورس</th>
+                <th>حالة التفعيل</th>
+                <th>تاريخ الاضافة</th>
+                <th>تاريخ التحديث</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($data as $info)
+              <tr>
+                <td>{{ $info->name }}</td>
+                <td>@if ( $info->active==1)مفعل @else معطل@endif</td>
+                <td>{{ $info->created_at }}</td>
+                <td>{{ $info->updated_at }}</td>
+                <td>
+                    <a href="{{ route('Corses.edit',$info->id) }}" class="button" style="background-color: green;padding: 10px;color: white">تعديل</a>
+                    <a href="{{ route('Corses.delete',$info->id) }}" class="button" style="background-color: rgb(212, 144, 179);padding: 10px;color: white">حذف</a>
+
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+          @else
+          <p style="text-align: center;color: brown;margin-top: 20px">لا توجد بيانات لعرضها</p>
+          @endif
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+    </div>
+ 
+@endsection
